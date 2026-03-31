ebb5d97b32fd3cf2e8c25aaed2afac4ab6b97810
carlosrobles13 <carlosrobles21@hotmail.com>
2026-03-31 18:59:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,42 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <meta http-equiv="Content-Style-Type" content="text/css">
-  <title></title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="2685.3">
-  <style type="text/css">
-    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica}
-    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; font: 12.0px Helvetica; min-height: 14.0px}
-  </style>
-</head>
-<body>
-<p class="p1">const canvas = document.getElementById("canvas");</p>
-<p class="p1">const ctx = canvas.getContext("2d");</p>
-<p class="p2"><br></p>
-<p class="p1">function resize() {</p>
-<p class="p1"><span class="Apple-converted-space">  </span>canvas.width = canvas.clientWidth;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>canvas.height = canvas.clientHeight;</p>
-<p class="p1">}</p>
-<p class="p1">window.addEventListener("resize", resize);</p>
-<p class="p1">resize();</p>
-<p class="p2"><br></p>
-<p class="p1">function sync(rangeId, numberId) {</p>
-<p class="p1"><span class="Apple-converted-space">  </span>const r = document.getElementById(rangeId);</p>
-<p class="p1"><span class="Apple-converted-space">  </span>const n = document.getElementById(numberId);</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>r.oninput = () =&gt; n.value = r.value;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>n.oninput = () =&gt; r.value = n.value;</p>
-<p class="p1">}</p>
-<p class="p2"><br></p>
-<p class="p1">sync("f1", "f1n");</p>
-<p class="p1">sync("phi1", "phi1n");</p>
-<p class="p1">sync("f2", "f2n");</p>
-<p class="p1">sync("phi2", "phi2n");</p>
-<p class="p2"><br></p>
-<p class="p1">function draw() {</p>
-<p class="p1"><span class="Apple-converted-space">  </span>const w = canvas.width;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>const h = canvas.height;</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>ctx.clearRect(0, 0, w, h);</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>const f1 = +f1n.value;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>const f2 = +f2n.value;</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>const p1 = (+phi1n.value * Math.PI) / 180;</p>
-<p class="p1"><span class="Apple-converted-space">  </span>const p2 = (+phi2n.value * Math.PI) / 180;</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>const center = h / 2;</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>ctx.beginPath();</p>
-<p class="p1"><span class="Apple-converted-space">  </span>for (let i = 0; i &lt; w; i++) {</p>
-<p class="p1"><span class="Apple-converted-space">    </span>const t = i / w * 10;</p>
-<p class="p1"><span class="Apple-converted-space">    </span>const y = Math.sin(t + p1);</p>
-<p class="p1"><span class="Apple-converted-space">    </span>ctx.lineTo(i, center - y * 60);</p>
-<p class="p1"><span class="Apple-converted-space">  </span>}</p>
-<p class="p1"><span class="Apple-converted-space">  </span>ctx.strokeStyle = "white";</p>
-<p class="p1"><span class="Apple-converted-space">  </span>ctx.stroke();</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>ctx.beginPath();</p>
-<p class="p1"><span class="Apple-converted-space">  </span>for (let i = 0; i &lt; w; i++) {</p>
-<p class="p1"><span class="Apple-converted-space">    </span>const t = i / w * 10 * (f2 / f1);</p>
-<p class="p1"><span class="Apple-converted-space">    </span>const y = Math.sin(t + p2);</p>
-<p class="p1"><span class="Apple-converted-space">    </span>ctx.lineTo(i, center - y * 60);</p>
-<p class="p1"><span class="Apple-converted-space">  </span>}</p>
-<p class="p1"><span class="Apple-converted-space">  </span>ctx.strokeStyle = "cyan";</p>
-<p class="p1"><span class="Apple-converted-space">  </span>ctx.stroke();</p>
-<p class="p2"><br></p>
-<p class="p1"><span class="Apple-converted-space">  </span>requestAnimationFrame(draw);</p>
-<p class="p1">}</p>
-<p class="p2"><br></p>
-<p class="p1">draw();</p>
-</body>
-</html>
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = canvas.offsetWidth;
+canvas.height = 300;
+
+const f1 = document.getElementById("f1");
+const phi1 = document.getElementById("phi1");
+const f2 = document.getElementById("f2");
+const phi2 = document.getElementById("phi2");
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const w = canvas.width;
+  const h = canvas.height;
+
+  ctx.beginPath();
+
+  for (let x = 0; x < w; x++) {
+    const t = x / 50;
+
+    const y1 = Math.sin(t * f1.value * 0.01 + phi1.value * Math.PI / 180);
+    const y2 = Math.sin(t * f2.value * 0.01 + phi2.value * Math.PI / 180);
+
+    const y = (y1 + y2) / 2;
+
+    const drawY = h / 2 + y * 80;
+
+    if (x === 0) ctx.moveTo(x, drawY);
+    else ctx.lineTo(x, drawY);
+  }
+
+  ctx.strokeStyle = "#22c55e";
+  ctx.stroke();
+}
+
+[f1, phi1, f2, phi2].forEach(el => {
+  el.addEventListener("input", draw);
+});
+
+draw();
